4396c592e713fd815a66b582945959e0f05d5341
Trygve Lie <post@trygve-lie.com>
2016-11-21 23:52:05 +0100

    hasing 100% with asset-pipe-common

--- test/sink.js
+++ test/sink.js
@@ -6,28 +6,21 @@ const tap = require('tap');
 const fs = require('fs');
 
 
-
-
-const sourceStream = (arr) => {
-    return new stream.Readable({
-        objectMode : false,
-        read: function (n) {
-            arr.forEach((chunk) => {
-                this.push(chunk);
-            });
-            this.push(null);
-        }
-    });
-}
-
-const a = ['a','b','c'];
-const b = ['d', 'a','b','c'];
-
-
 tap.test('not a real test', (t) => {
     const sink = new Sink('./');
     const file = fs.createReadStream('./test/mock/feed.a.json');
     const dest = sink.writer('json', (filename) => {
+        console.log(filename);
+        t.end();
+    });
+    file.pipe(dest);
+});
+
+
+tap.test('another not a real test', (t) => {
+    const sink = new Sink('./');
+    const file = fs.createReadStream('./test/mock/feed.a.json');
+    const dest = sink.writer('js', (filename) => {
         console.log(filename);
         t.end();
     });
