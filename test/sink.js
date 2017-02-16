1c066adcec836eaa3a1c391cf23070718a6b5454
sveisvei <sveinung.rosaker@gmail.com>
2017-02-16 11:48:50 +0100

    use tmpdir instead in tests

--- test/sink.js
+++ test/sink.js
@@ -3,10 +3,11 @@
 const Sink = require('../');
 const tap = require('tap');
 const fs = require('fs');
+const { tmpdir } = require('os');
 
 
 tap.test('not a real test', (t) => {
-    const sink = new Sink('./');
+    const sink = new Sink(tmpdir());
     const file = fs.createReadStream('./test/mock/feed.a.json');
     const dest = sink.writer('json', (filename) => {
         console.log(filename);
@@ -17,7 +18,7 @@ tap.test('not a real test', (t) => {
 
 
 tap.test('another not a real test', (t) => {
-    const sink = new Sink('./');
+    const sink = new Sink(tmpdir());
     const file = fs.createReadStream('./test/mock/feed.a.json');
     const dest = sink.writer('js', (filename) => {
         console.log(filename);
