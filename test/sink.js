449e4e3cb5a011e71a9ea00e0eb12e86715fcc9f
Trygve Lie <post@trygve-lie.com>
2017-03-02 10:20:51 +0100

    Rewrite of .writer() to emit events on upload. Started adding tests for .writer()

--- test/sink.js
+++ test/sink.js
@@ -3,31 +3,90 @@
 const stream = require('readable-stream');
 const Sink = require('../');
 const tap = require('tap');
+const fs = require('fs');
+const os = require('os');
 
 
-tap.test('constructor() - no value for "fileDir" argument - should throw', (t) => {
+tap.test('constructor() - no value for "options.path" argument - should throw', (t) => {
     t.throws(() => {
         new Sink(); // eslint-disable-line
-    }, new Error('"fileDir" is missing'));
+    }, new Error('"options.path" is missing'));
     t.end();
 });
 
-tap.test('constructor() - has value for "fileDir" argument - should be of Sink Class type', (t) => {
-    t.type(new Sink('./'), Sink);
+tap.test('constructor() - has value for "options.path" argument - should be of Sink Class type', (t) => {
+    t.type(new Sink({
+        path: './',
+    }), Sink);
     t.end();
 });
 
 
-tap.test('.reader() - no value for "fileName" argument - should throw', (t) => {
-    const sink = new Sink('./test/mock/');
+tap.test('.writer() - no value for "type" argument - should throw', (t) => {
+    const sink = new Sink({
+        path: './test/.tmp/',
+    });
+    t.throws(() => {
+        const dest = sink.writer(); // eslint-disable-line
+    }, new Error('"type" is missing'));
+    t.end();
+});
+
+tap.test('.writer() - save to non existing path - should emit "file not saved" event', (t) => {
+    const sink = new Sink({
+        path: './test/.nonExistingDirectory/',
+    });
+    const source = fs.createReadStream('./test/mock/feed.a.json');
+    const dest = sink.writer('json');
+    dest.on('file not saved', () => {
+        t.assert(true);
+        t.end();
+    });
+    source.pipe(dest);
+});
+
+tap.test('.writer() - save to existing path - should emit "file saved" event', (t) => {
+    const sink = new Sink({
+        path: os.tmpdir(),
+    });
+    const source = fs.createReadStream('./test/mock/feed.a.json');
+    const dest = sink.writer('json');
+    dest.on('file saved', () => {
+        t.assert(true);
+        t.end();
+    });
+    source.pipe(dest);
+});
+
+tap.test('.writer() - on "file saved" - should have "id" and "file" on emitted event', (t) => {
+    const sink = new Sink({
+        path: os.tmpdir(),
+    });
+    const source = fs.createReadStream('./test/mock/feed.a.json');
+    const dest = sink.writer('json');
+    dest.on('file saved', (id, file) => {
+        t.assert(id);
+        t.assert(file);
+        t.end();
+    });
+    source.pipe(dest);
+});
+
+
+tap.test('.reader() - no value for "file" argument - should throw', (t) => {
+    const sink = new Sink({
+        path: './test/mock/',
+    });
     t.throws(() => {
         const source = sink.reader(); // eslint-disable-line
-    }, new Error('"fileName" is missing'));
+    }, new Error('"file" is missing'));
     t.end();
 });
 
 tap.test('.reader() - read non-existing file - should emit "file not found" event', (t) => {
-    const sink = new Sink('./test/mock/');
+    const sink = new Sink({
+        path: './test/mock/',
+    });
     const source = sink.reader('feed.b.json');
     source.on('file not found', () => {
         t.assert(true);
@@ -36,7 +95,9 @@ tap.test('.reader() - read non-existing file - should emit "file not found" even
 });
 
 tap.test('.reader() - read non-existing file - should emit (inherited) "error" event', (t) => {
-    const sink = new Sink('./test/mock/');
+    const sink = new Sink({
+        path: './test/mock/',
+    });
     const source = sink.reader('feed.b.json');
     source.on('error', () => {
         t.assert(true);
@@ -45,7 +106,9 @@ tap.test('.reader() - read non-existing file - should emit (inherited) "error" e
 });
 
 tap.test('.reader() - read existing file - should emit "file found" event', (t) => {
-    const sink = new Sink('./test/mock/');
+    const sink = new Sink({
+        path: './test/mock/',
+    });
     const source = sink.reader('feed.a.json');
     source.on('file found', () => {
         t.assert(true);
@@ -54,7 +117,9 @@ tap.test('.reader() - read existing file - should emit "file found" event', (t) 
 });
 
 tap.test('.reader() - read existing file - should emit (inherited) "open" event', (t) => {
-    const sink = new Sink('./test/mock/');
+    const sink = new Sink({
+        path: './test/mock/',
+    });
     const source = sink.reader('feed.a.json');
     source.on('open', () => {
         t.assert(true);
@@ -63,7 +128,9 @@ tap.test('.reader() - read existing file - should emit (inherited) "open" event'
 });
 
 tap.test('.reader() - read existing file - should emit (inherited) "open" event', (t) => {
-    const sink = new Sink('./test/mock/');
+    const sink = new Sink({
+        path: './test/mock/',
+    });
     const source = sink.reader('feed.a.json');
     source.on('open', () => {
         t.assert(true);
@@ -80,7 +147,9 @@ tap.test('.reader() - read existing file - should stream read file', (t) => {
         },
     });
 
-    const sink = new Sink('./test/mock/');
+    const sink = new Sink({
+        path: './test/mock/',
+    });
     const source = sink.reader('feed.a.json');
 
     source.on('file found', () => {
